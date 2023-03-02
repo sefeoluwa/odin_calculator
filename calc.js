8b14063c2ec5ecb2b16be0b7cb78e0ccb4d4f4de
sefeoluwa <sefeoluwa01@gmail.com>
2023-03-02 10:22:22 +0100

    DOM selectors

---
+++ calc.js
@@ -0,0 +1,10 @@
+//DOM manipulation selectors
+const entry = document.querySelector('#entry-display');
+const result = document.querySelector('#result');
+const clear = document.querySelector('#clear');
+const del = document.querySelector('#del');
+const divide = document.querySelector('#divide');
+const multiply = document.querySelector('#multiply');
+const subtract = document.querySelector('#subtract');
+const add = document.querySelector('#add');
+const equals = document.querySelector('#equals');
